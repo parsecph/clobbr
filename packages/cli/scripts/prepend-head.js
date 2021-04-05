f7bc5b1202cd8b7ac1335b52154403212dc1d03a
Dan Mindru <nadurdnim@icloud.com>
2021-04-05 21:15:30 +0200

    Better prepend

--- packages/cli/scripts/prepend-head.js
+++ packages/cli/scripts/prepend-head.js
@@ -3,5 +3,6 @@ const prependFile = require('prepend-file');
 const entry = path.resolve(__dirname, '../dist/index.js');
 
 (async () => {
-  await prependFile(entry, '#!/usr/bin/env node');
-});
+  await prependFile(entry, '#!/usr/bin/env node\n');
+  console.log('Preprended headers')
+})();
