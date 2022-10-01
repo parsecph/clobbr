4fafbcd11683ee14ebd25a1643f3da12c8553bab
Dan Mindru <mindrudan@gmail.com>
2022-10-01 14:31:55 +0200

    Customize user agent

--- packages/api/src/api.ts
+++ packages/api/src/api.ts
@@ -1,5 +1,6 @@
 import { AxiosInstance } from 'axios';
 import { DEFAULT_HTTP_TIMEOUT_IN_MS } from './consts/http';
+import packageJson from '../package.json';
 
 const http = require('axios');
 
@@ -12,5 +13,6 @@ const api = {
 api.http.defaults.timeout = DEFAULT_HTTP_TIMEOUT_IN_MS;
 api.http.defaults.timeoutErrorMessage = 'Request timed out (10s)';
 api.http.defaults.maxRedirects = 99;
+api.http.defaults.headers['User-Agent'] = `clobbr/${packageJson.version}`;
 
 export default api;
