30880b23335849d0c1d0e5c9c0f378fafa81b1bc
Dan Mindru <mindrudan@gmail.com>
2023-01-07 22:00:59 +0100

    Reorder https enums

--- packages/api/src/enums/http.ts
+++ packages/api/src/enums/http.ts
@@ -2,18 +2,18 @@ export enum Everbs {
   GET = 'get',
   POST = 'post',
   PUT = 'put',
+  PATCH = 'patch',
   DELETE = 'delete',
   OPTIONS = 'options',
-  HEAD = 'head',
-  PATCH = 'patch'
+  HEAD = 'head'
 }
 
 export const VERBS = {
   GET: Everbs.GET,
   POST: Everbs.POST,
   PUT: Everbs.PUT,
+  PATCH: Everbs.PATCH,
   DELETE: Everbs.DELETE,
   OPTIONS: Everbs.OPTIONS,
-  HEAD: Everbs.HEAD,
-  PATCH: Everbs.PATCH
+  HEAD: Everbs.HEAD
 };
