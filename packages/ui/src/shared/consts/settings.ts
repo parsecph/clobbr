a89ff9bba7b26d9f63149208c71ff39b2ef7ebdd
Dan Mindru <mindrudan@gmail.com>
2023-07-20 17:42:51 +0200

    Fix support emails

--- packages/ui/src/shared/consts/settings.ts
+++ packages/ui/src/shared/consts/settings.ts
@@ -4,7 +4,7 @@ const sanitizeMailBody = (body: string) =>
   body.replace(/(?:\r\n|\r|\n)/g, '%0D%0A');
 
 export const MAX_ITERATIONS = 100;
-export const SUPPORT_EMAIL = '04.scale.emus@icloud.com';
+export const SUPPORT_EMAIL = 'support@clobbr.app';
 
 export const APP_INFO_TEXT = `
 
@@ -21,14 +21,14 @@ const BUG_REPORT_SUBJECT = '[Clobbr] Bug Report';
 const FEATURE_REQUEST_SUBJECT = '[Clobbr] Feature Request';
 const HELP_SUBJECT = '[Clobbr] Help';
 
-export const BUG_REPORT_HREF = `mailto:${SUPPORT_EMAIL}1?subject=${BUG_REPORT_SUBJECT}&body=${sanitizeMailBody(
+export const BUG_REPORT_HREF = `mailto:${SUPPORT_EMAIL}?subject=${BUG_REPORT_SUBJECT}&body=${sanitizeMailBody(
   APP_INFO_TEXT
 )}`;
 
-export const FEATURE_REQUEST_HREF = `mailto:${SUPPORT_EMAIL}2?subject=${FEATURE_REQUEST_SUBJECT}&body=${sanitizeMailBody(
+export const FEATURE_REQUEST_HREF = `mailto:${SUPPORT_EMAIL}?subject=${FEATURE_REQUEST_SUBJECT}&body=${sanitizeMailBody(
   APP_INFO_TEXT
 )}`;
 
-export const HELP_HREF = `mailto:${SUPPORT_EMAIL}3?subject=${HELP_SUBJECT}&body=${sanitizeMailBody(
+export const HELP_HREF = `mailto:${SUPPORT_EMAIL}?subject=${HELP_SUBJECT}&body=${sanitizeMailBody(
   APP_INFO_TEXT
 )}`;
