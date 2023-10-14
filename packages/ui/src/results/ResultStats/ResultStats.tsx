812d44a4de527f19fba824f751ce2dbca23a6649
Dan Mindru <mindrudan@gmail.com>
2023-10-14 22:11:31 +0200

    Handle no statistics in chronoligcal view

--- packages/ui/src/results/ResultStats/ResultStats.tsx
+++ packages/ui/src/results/ResultStats/ResultStats.tsx
@@ -33,7 +33,11 @@ export const ResultStats = ({
   ]);
 
   if (!stats) {
-    return <></>;
+    return (
+      <Typography className="!text-xs opacity-50 p-2">
+        No statistics available
+      </Typography>
+    );
   }
 
   return (
